b4a9fa94f243fa9ede51009fc0528759c586bfaa
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2020-02-26 13:53:14 +0000

    Update jump_game1.py

--- jump_game1.py
+++ jump_game1.py
@@ -1,5 +1,8 @@
 import numpy,math,pygame
-
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 class Player():
     def __init__(self):
         super().__init__()
@@ -8,23 +11,27 @@ class Player():
         self.rect = self.image.get_rect()
         self.direction = 'up'
     def update(self):
-        if direction == 'up':
+        if self.direction == 'up':
             self.rect.y -= speed
-        elif direction == 'down':
+        elif self.direction == 'down':
             self.rect.y += speed
+        if self.rect.y > 500:
+            self.direction = 'up'
         self.change_speed()
     def change_speed(self):
-        
+        if self.direction == 'up':
+            self.speed -= 1
+        elif self.direction == 'down':
+            self.speed += 1
         
 
 
 class Computer(Player):
-
+    pass
 class Block():
-
+    pass
 class Alien():
-
-
+    pass
     
 def gameplay():
     player = Player()
@@ -45,16 +52,22 @@ def gameplay():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
-            player.move('right')
+            player.rect.x += 2
         elif keys[pygame.K_LEFT]:
-            player.move('left')
+            player.rect.x -= 2
 
 
 
         screen.fill(BLACK)
-
+        player.update()
+        player.draw()
         pygame.display.flip()
 
         clock.tick(60)
 
-        
+
+pygame.init()
+size = (700,500)
+screen = pygame.display.set_mode(size)
+gameplay()
+pygame.quit()
