cf8e336cd1d930d1623c65189c2ed2f6f8d4e62c
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2020-02-27 09:45:10 +0000

    Update jump_game1.py

--- jump_game1.py
+++ jump_game1.py
@@ -3,39 +3,67 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-class Player():
+class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.Surface([20,20])
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
+        self.rect.x = size[0]//2
+        self.rect.y = size[1] - 20
         self.direction = 'up'
+        self.speed = 20
+        self.terminal_velocity = 23
     def update(self):
         if self.direction == 'up':
-            self.rect.y -= speed
+            self.rect.y -= self.speed
         elif self.direction == 'down':
-            self.rect.y += speed
-        if self.rect.y > 500:
-            self.direction = 'up'
+            self.rect.y += self.speed
+        if self.rect.y > 480:
+            self.reverse()
+            self.rect.y = 480
         self.change_speed()
     def change_speed(self):
+        if self.speed == 0:
+            self.reverse
         if self.direction == 'up':
             self.speed -= 1
         elif self.direction == 'down':
             self.speed += 1
-        
-
-
+    def reverse(self):
+        print(self.speed)
+        if self.direction == 'up':
+            self.direction = 'down'
+        elif self.direction == 'down':
+            self.direction = 'up'
+            if self.speed < 18
+        if self.speed > self.terminal_velocity:
+            self.speed = self.terminal_velocity
+            
 class Computer(Player):
     pass
-class Block():
-    pass
+class Block(pygame.sprite.Sprite):
+    def __init__(self,position):
+        super().__init__()
+        self.image = pygame.Surface([80,20])
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = position[0]
+        self.rect.y = position[1]
+        
+   
 class Alien():
     pass
     
 def gameplay():
+    all_sprites_group = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    block_group = pygame.sprite.Group()
     player = Player()
-
+    test_block = Block([size[0]//2,400])
+    block_group.add(test_block)
+    player_group.add(player)
+    all_sprites_group.add(player,test_block)
     game_over = False
     clock = pygame.time.Clock()
 
@@ -52,15 +80,16 @@ def gameplay():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
-            player.rect.x += 2
+            player.rect.x += 5
         elif keys[pygame.K_LEFT]:
-            player.rect.x -= 2
-
-
+            player.rect.x -= 5
+        if pygame.sprite.spritecollide(player,block_group,False):
+            player.reverse()
 
         screen.fill(BLACK)
         player.update()
-        player.draw()
+        player_group.draw(screen)
+        block_group.draw(screen)
         pygame.display.flip()
 
         clock.tick(60)
