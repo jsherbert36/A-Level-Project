cf5f2b68ef99680715bf90600f015fa45c18e586
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2020-03-07 09:48:04 +0000

    add iteration 4 and restrucutre game for multiple players

--- Iteration4/Basic_Game_4.py
+++ Iteration4/Basic_Game_4.py
@@ -26,8 +26,11 @@ class Player(pygame.sprite.Sprite):
         self.terminal_velocity = 22
         self.height = 0
         self.score = 0
+        self.current_block = 0
+        self.current_pos = [size[0]//2,size[1] - 20]
 
     def update(self):
+        print('update')
         self.image_num = (self.image_num + 1 ) % 14
         if self.image_num % 3 == 0:
             self.image = self.image_list[self.image_num//2]
@@ -42,6 +45,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.x = size[0] - self.width
         if self.speed > self.terminal_velocity:
             self.speed = self.terminal_velocity
+
     def change_speed(self):
         if self.speed == 0:
             self.reverse()
@@ -49,21 +53,42 @@ class Player(pygame.sprite.Sprite):
             self.speed -= 1
         elif self.direction == 'down':
             self.speed += 1
+
     def reverse(self):
         if self.direction == 'up':
             self.direction = 'down'
         elif self.direction == 'down':
             self.direction = 'up'
             self.speed = self.normal_speed
+
+    def collision(self,block_group,start_block):
+        block_hit_list = pygame.sprite.spritecollide(self,block_group,False)
+        for block in block_hit_list:
+            if self.rect.bottom > block.rect.top and self.direction == 'down':
+                self.current_block = block.number
+                self.current_pos = block.rect.center
+                self.reverse()
+        if self.rect.colliderect(start_block):
+            if self.rect.bottom > start_block.rect.top and self.direction == 'down':
+                self.reverse()
+
+    def set_score(self,start_block):
+        self.height = start_block.rect.top - self.rect.bottom 
+        if self.height > self.score:
+            self.score = self.height
         
+    def set_next(self,block_group):
+        for block in block_group:
+            if block.number == self.current_block + 1:
+                self.next_pos = block.rect.topleft
+                
             
 class Computer(Player):
     pass
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self,position,block_width,number,Type = 'Still'):
+    def __init__(self,position,block_width,number):
         super().__init__()
-        self.Type = Type
         self.number = number
         self.position = position
         self.block_width = block_width
@@ -72,45 +97,8 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = self.position[0]
         self.rect.y = self.position[1]
-        self.direction = None
     def update(self):
         pass
-
-class SideBlock(Block):
-    def __init__(self,position,block_width,number):
-        super().__init__(position,block_width,number,'Horizontal')
-        self.direction = random.choice(['left','right'])
-        self.distance = 120
-        if self.position[0] > size[0] - self.distance - self.block_width:
-            self.position[0] = size[0] - self.distance - self.block_width
-        elif self.position[0] < self.distance + block_width//2:
-            self.position[0] = self.distance + block_width//2
-    def update(self):
-        if self.rect.centerx > self.position[0] + self.distance:
-            self.direction = 'left'
-        elif self.rect.centerx < self.position[0] - self.distance:
-            self.direction = 'right'
-        if self.direction == 'left':
-            self.rect.x -= 2
-        elif self.direction == 'right':
-            self.rect.x += 2
-
-
-class VerticalBlock(Block):
-    def __init__(self,position,block_width,distance,number):
-        super().__init__(position,block_width,number,'Vertical')
-        self.direction = random.choice(['up','down'])
-        self.distance = distance
-        
-    def update(self):
-        if self.rect.centery < self.position[1] - self.distance:
-            self.direction = 'down'
-        elif self.rect.centery > self.position[1] + self.distance:
-            self.direction = 'up'
-        if self.direction == 'up':
-            self.rect.y -= 1
-        elif self.direction == 'down':
-            self.rect.y += 1
 
         
 class Alien():
@@ -119,21 +107,21 @@ class Alien():
 def move(direction,block_y,sprite_group):
     for sprite in sprite_group:
         sprite.rect.y += direction
-        if isinstance(sprite,VerticalBlock):
-            sprite.position[1] += direction 
     block_y += direction
     return block_y
     
     
 def gameplay():
-    score_font = pygame.font.Font(os.path.join(PATH,"Score_Font.ttf"), 40) 
+    #score_font = pygame.font.Font(os.path.join(PATH,"Score_Font.ttf"), 35)
+    score_font = pygame.font.Font("freesansbold.ttf", 35) 
     all_sprites_group = pygame.sprite.Group()
     player_group = pygame.sprite.Group()
     block_group = pygame.sprite.Group()
     block_count = 0
-    player = Player()
+    player1 = Player()
+    player_group.add(player1)
+    all_sprites_group.add(player1)
     block_width = 90
-    vertical_distance = 60
     block_y = size[1] - 20
     block_x = random.randint(80,size[0] - 80)
     start_block = Block([0,block_y],size[0],block_count)
@@ -146,14 +134,8 @@ def gameplay():
         block_count += 1
         block_group.add(new_block)
         all_sprites_group.add(new_block)
-    player_group.add(player)
-    all_sprites_group.add(player)
     game_over = False
     clock = pygame.time.Clock()
-    current_block = 0
-    current_pos = [size[0]//2,size[1] - 20]
-    current_centre = [0,0]
-    current_type = 'Still'
     horizontal_direction = 'none'
    
     
@@ -164,61 +146,36 @@ def gameplay():
             if event.type == pygame.QUIT:
                 game_over = True
                 pygame.quit()
-                return 'gameover'
+                return 'exit'
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     game_over = True
+                    return 'gameover'
         
-        player.height = start_block.rect.top - player.rect.bottom 
+        player1.set_score(start_block)
 
-        if player.height > player.score:
-            player.score = player.height
-
-        for block in block_group:
-            if block.number == current_block + 1:
-                next_pos = block.rect.topleft
-                next_type = block.Type
-                if block.Type == 'Still':
-                    next_centre = block.rect.center
-                else: 
-                    next_centre = block.position
-                next_direction = block.direction 
+        player1.set_next(block_group) 
         
-        horizontal_direction = Test_API.move(player.rect.center,current_pos,next_pos,next_type,next_centre,player.normal_speed,player.speed,size,player.direction,next_direction,block_width,horizontal_direction,current_type,current_centre)
+        horizontal_direction = 'none'
         if horizontal_direction == 'right':
-            player.rect.x += 9
+            player1.rect.x += 9
         elif horizontal_direction == 'left':
-            player.rect.x -= 9
+            player1.rect.x -= 9
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            player1.rect.x += 9
+        elif keys[pygame.K_LEFT]:
+            player1.rect.x -= 9
         
-        block_hit_list = pygame.sprite.spritecollide(player,block_group,False)
-        for block in block_hit_list:
-            
-            if player.rect.bottom > block.rect.top and player.direction == 'down':
-                current_block = block.number
-                current_pos = block.rect.center
-                current_type = block.Type
-                current_centre = block.position
-                player.reverse()
-        if player.rect.colliderect(start_block):
-            if player.rect.bottom > start_block.rect.top and player.direction == 'down':
-                player.reverse()
-        if player.rect.y < size[0]//4 :            
+        player1.collision(block_group,start_block)
+
+        if player1.rect.y < size[0]//4 :            
             if block_y > 0:
                 temp_y = random.randint(50,100)
                 block_y -= temp_y
                 block_x = (block_x + (random.randint(-340,340))) % (size[0] - block_width)
-                if block_y < 300:
-                    block_type = random.choice(['move','still','still','still','still','vertical'])
-                else:
-                    block_type = 'still'
-                if block_type == 'move':
-                    new_block = SideBlock([block_x,block_y], block_width,block_count)
-                elif block_type == 'still':
-                    new_block = Block([block_x,block_y], block_width,block_count)
-                elif block_type == 'vertical':
-                    block_y -= (vertical_distance + temp_y)
-                    new_block = VerticalBlock([block_x,block_y], block_width, vertical_distance,block_count)
-                    block_y -= vertical_distance
+                new_block = Block([block_x,block_y], block_width,block_count)
                 block_count += 1
                 block_group.add(new_block)
                 all_sprites_group.add(new_block)
@@ -228,10 +185,10 @@ def gameplay():
             if block.rect.y > size[1] + 50:
                 block.kill()
         
-        if player.rect.bottom > size[1]:
+        if player1.rect.bottom > size[1]:
             game_over = True
             return 'gameover'
-            #player.reverse()
+            #player1.reverse()
 
             
         screen.blit(background_image_1,(0,0))
@@ -239,7 +196,7 @@ def gameplay():
         player_group.draw(screen)
         block_group.draw(screen)
 
-        score_display = score_font.render(str(player.score), True, BLACK)
+        score_display = score_font.render(str(player1.score), True, BLACK)
         screen.blit(score_display,(size[0]//20, size[1]//20))
 
         pygame.display.flip()
@@ -260,7 +217,7 @@ def gameover():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
-    time.sleep(1)
+    #time.sleep(1)
 
 
 if __name__ == '__main__':
@@ -272,4 +229,6 @@ if __name__ == '__main__':
     while True:
         if gameplay() == 'gameover':
             gameover()
+        else:
+            break
 
