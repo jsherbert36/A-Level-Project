dc1c6bdc8c74e53e890f962e798832a487d20e11
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2020-04-16 09:50:06 +0100

    add menu

--- Iteration8/Full_Game8.py
+++ Iteration8/Full_Game8.py
@@ -1,4 +1,4 @@
-import math,pygame,random,sys,os,GamePlay,GameOver
+import math,pygame,random,sys,os,Menu,GameOver,GamePlay
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -7,6 +7,13 @@ PATH = sys.path[0]
 import numpy as np
 
 if __name__ == "__main__":
-    GamePlay.gameplay(SIZE,screen)
-    
-    
+    pygame.init()
+    SIZE = (1280,720)
+    screen = pygame.display.set_mode(SIZE)
+    choice = Menu.Menu1(SIZE,screen)
+    if choice != 'gameover':
+        if choice == 'single':
+            GamePlay.gameplay(SIZE,screen)
+        elif choice == 'double':
+            pass
+    pygame.quit()
--- Iteration8/Menu.py
+++ Iteration8/Menu.py
@@ -1,25 +1,28 @@
+import math,pygame,random,sys,os
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (204,0,0)
+PATH = sys.path[0]
+
 class Button():
-    Button.WHITE = (255,255,255)
-    Button.BLACK = (0,0,0)
-    Button.RED = (204,0,0)
-    def __init__(y_pos,size,text):
+    def __init__(self,y_pos,size,text):
         self.size = size
         self.font = pygame.font.Font('freesansbold.ttf',self.size)
-        self.colour = Button.WHITE
+        self.colour = WHITE
         self.text = text
         self.object = self.font.render(self.text, True, self.colour)
         self.rect = self.object.get_rect()
-        self.rect.x = SIZE[0]//2
-        self.rect.y = y_pos
+        self.rect.centerx = SIZE[0]//2
+        self.rect.centery = y_pos
 
     def draw(self):
         screen.blit(self.object,self.rect)
 
     def update(self,mouse):
         if self.rect.collidepoint(mouse):
-            self.colour = Button.RED
+            self.colour = RED
         else:
-            self.colour = Button.WHITE
+            self.colour = WHITE
         self.object = self.font.render(self.text, True, self.colour)
 
 def Menu1(window,surface):
@@ -27,29 +30,36 @@ def Menu1(window,surface):
     SIZE = window
     screen = surface
     choice = False
+    button_size = 70
     button_list = []
+    button_list.append(Button(SIZE[1]//3.5,button_size,'Single Player'))
+    button_list.append(Button((SIZE[1]//3.5) * 2,button_size,'Two Player'))
+    background_image_1 = pygame.image.load(os.path.join(PATH,"images","Background.jpg")).convert()
+    background_image_1 = pygame.transform.smoothscale(background_image_1, SIZE)
+    clock = pygame.time.Clock()
 
-    while not game_over and not choice:
-        screen.fill(BLACK)  
+    while not choice:
+        screen.blit(background_image_1,(0,0)) 
         mouse = pygame.mouse.get_pos()      
-        
-        pygame.display.flip()
+        for button in button_list:
+            button.update(mouse)
+            button.draw()
 
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    game_over = True
+                    choice = True
+                    return 'gameover'
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
-                        game_over = True
+                        choice = True
+                        return 'gameover'
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    mouse = pygame.mouse.get_pos()
-                    if SingleRect.collidepoint(mouse):
+                    if button_list[0].rect.collidepoint(mouse):
                         choice = True
                         return 'single'
-                    elif MultiRect.collidepoint(mouse):
+                    elif button_list[1].rect.collidepoint(mouse):
                         choice = True
-                        return 'multi'
-                    elif ScoreRect.collidepoint(mouse):
-                        choice = True
-                        return 'score'     
+                        return 'double'
+
+        pygame.display.flip()       
         clock.tick(60)
